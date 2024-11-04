32075c9952570282d55419decd5f08663a9e5597
uxshane <uxchampaign@gmail.com>
2024-11-04 19:38:40 +0900

    .gitkeep files added

--- src/main/java/com/example/realestate_api/transaction/transaction_entity/RealEstateTransaction.java
+++ src/main/java/com/example/realestate_api/transaction/transaction_entity/RealEstateTransaction.java
@@ -17,100 +17,101 @@ public class RealEstateTransaction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "sgg_cd")
+    @Column(name = "sgg_cd", length = 5)
     private String sggCd;
 
-    @Column(name = "umd_cd")
+    @Column(name = "umd_cd", length = 5)
     private String umdCd;
 
-    @Column(name = "land_cd")
+    @Column(name = "land_cd", length = 1)
     private String landCd;
 
-    @Column(name = "bonbun")
+    @Column(name = "bonbun", length = 4)
     private String bonbun;
 
-    @Column(name = "bubun")
+    @Column(name = "bubun", length = 4)
     private String bubun;
 
-    @Column(name = "road_nm")
+    @Column(name = "road_nm", length = 100)
     private String roadNm;
 
-    @Column(name = "road_nm_sgg_cd")
+    //ssgCd, lawdCd와 중복된 값
+    @Column(name = "road_nm_sgg_cd", length = 5)
     private String roadNmSggCd;
 
-    @Column(name = "road_nm_cd")
+    @Column(name = "road_nm_cd", length = 7)
     private String roadNmCd;
-
-    @Column(name = "road_nm_seq")
+    
+    @Column(name = "road_nm_seq", length = 2)
     private String roadNmSeq;
 
-    @Column(name = "road_nmb_cd")
+    @Column(name = "road_nmb_cd", length = 1)
     private String roadNmbCd;
 
-    @Column(name = "road_nm_bonbun")
+    @Column(name = "road_nm_bonbun", length = 5)
     private String roadNmBonbun;
 
-    @Column(name = "road_nm_bubun")
+    @Column(name = "road_nm_bubun", length = 5)
     private String roadNmBubun;
 
-    @Column(name = "umd_nm")
+    @Column(name = "umd_nm", length = 60)
     private String umdNm;
 
-    @Column(name = "apt_name")
+    @Column(name = "apt_name", length = 100)
     private String aptNm;
 
-    @Column(name = "jibun")
+    @Column(name = "jibun", length = 20)
     private String jibun;
 
-    @Column(name = "exclu_use_ar")
+    @Column(name = "exclu_use_ar", length = 22)
     private double excluUseAr;
 
-    @Column(name = "deal_year")
+    @Column(name = "deal_year", length = 4)
     private String dealYear;
 
-    @Column(name = "deal_month", length = 10)
+    @Column(name = "deal_month", length = 2)
     private String dealMonth;
 
-    @Column(name = "deal_day", length = 10)
+    @Column(name = "deal_day", length = 2)
     private String dealDay;
 
-    @Column(name = "deal_amount", length = 100)
+    @Column(name = "deal_amount", length = 40)
     private String dealAmount;
 
-    @Column(name = "floor")
+    @Column(name = "floor", length = 10)
     private int floor;
 
-    @Column(name = "build_year")
+    @Column(name = "build_year", length = 4)
     private int buildYear;
 
-    @Column(name = "apt_seq")
+    @Column(name = "apt_seq", length = 20)
     private String aptSeq;
 
-    @Column(name = "cdeal_type")
+    @Column(name = "cdeal_type", length = 1)
     private String cdealType;
 
-    @Column(name = "cdeal_day")
+    @Column(name = "cdeal_day", length = 8)
     private String cdealDay;
 
-    @Column(name = "dealing_gbn")
+    @Column(name = "dealing_gbn", length = 10)
     private String dealingGbn;
 
-    @Column(name = "estate_agent_sgg_nm")
+    @Column(name = "estate_agent_sgg_nm", length = 3000)
     private String estateAgentSggNm;
 
-    @Column(name = "rgst_date")
+    @Column(name = "rgst_date", length = 8)
     private String rgstDate;
 
-    @Column(name = "apt_dong")
+    @Column(name = "apt_dong", length = 400)
     private String aptDong;
 
-    @Column(name = "sler_gbn")
+    @Column(name = "sler_gbn", length = 100)
     private String slerGbn;
 
-    @Column(name = "buyer_gbn")
+    @Column(name = "buyer_gbn", length = 100)
     private String buyerGbn;
 
-    @Column(name = "land_leasehold_gbn")
+    @Column(name = "land_leasehold_gbn", length = 1)
     private String landLeaseholdGbn;
 
     @Column(name = "lawd_cd")
